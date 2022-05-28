3e591732cdc1a27513b39854a111751f8d0075f7
Jason Manuel <jama.indo@hotmail.com>
2022-05-27 23:41:27 -0700

    Integrate the reveal.js code highlighting plugin

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import Reveal from './node_modules/reveal.js/dist/reveal.esm.js';
 import Markdown from './node_modules/reveal.js/plugin/markdown/markdown.esm.js';
 import Notes from './node_modules/reveal.js/plugin/notes/notes.esm.js';
+import Highlight from './node_modules/reveal.js/plugin/highlight/highlight.esm.js';
 
 let deck = new Reveal({
-   plugins: [ Markdown, Notes ]
+   plugins: [ Markdown, Notes, Highlight ]
 });
 deck.initialize();
