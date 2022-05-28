c051c9bdc27b7df86727bc974bf3b070d5c947b8
Jason Manuel <jama.indo@hotmail.com>
2022-05-27 23:26:43 -0700

    Integrate the reveal.js speaker notes plugin
    Since the plugin requires using a web server, `yarn start`
    (which runs the `serve` package) should be used for
    local development.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import Reveal from './node_modules/reveal.js/dist/reveal.esm.js';
 import Markdown from './node_modules/reveal.js/plugin/markdown/markdown.esm.js';
+import Notes from './node_modules/reveal.js/plugin/notes/notes.esm.js';
 
 let deck = new Reveal({
-   plugins: [ Markdown ]
-})
+   plugins: [ Markdown, Notes ]
+});
 deck.initialize();
